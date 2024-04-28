6fb9eacf939c1c62ab5738805093c60343796d0f
PRAKASH <prakashkandasamy4@gmail.com>
2024-04-28 23:18:28 +0530

    Almost Done

--- Portfolio's/Portfolio-1/asset/script.js
+++ Portfolio's/Portfolio-1/asset/script.js
@@ -1,1 +1,25 @@
+var typed = new Typed(".typing", {
+    strings:["MERN Stack Developer"," Full Stack Developer", "React Developer", "Web Developer"],
+    typeSpeed: 100,
+    BackSpeed: 60, 
+    loop: true
+});
 
+
+
+
+// const nav = document.querySelector(".nav"),
+//       navList = nav.querySelectorAll("li"),
+//       totalNavList = navList.length;
+//       for(let i=0; i<totalNavList;i++)
+//       {
+//         const a =navList[i].querySelector("a");
+//         a.addEventListener("click", function()
+//     {
+//         for(let j=0; j<totalNavList; j++)
+//         {
+//             navList[j].querySelector("a").classList.remove("active")
+//         }
+//         this.classList.add("active")
+//     })
+//       }
