56864b5015b565769fdea8a7a0217ee11c45f20c
prakash-it <prakashkandasamy4@gmail.com>
2024-07-18 09:37:03 +0530

    Start

--- Auth-Login/Front-End/src/App.jsx
+++ Auth-Login/Front-End/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Signin from './Pages/Signin'
+import Signup from './Pages/Signup'
+import Profile from './Pages/Profile'
 function App() {
   return (
-    <div>App</div>
+   <BrowserRouter>
+  <Routes>   
+   <Route path='/' element={<Home/>} />
+   <Route path='/About' element={<About/>}/>
+   <Route path='/signin' element={<Signin/>}/>
+   <Route path='signup' element={<Signup/>}/>
+   <Route path='/profile' element={<Profile/>}/>
+   </Routes>
+   </BrowserRouter>
   )
 }
 
