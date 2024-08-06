6b9d2f47911299f7153d53ce2a5636f3f94630d5
prakash-it <prakashkandasamy4@gmail.com>
2024-08-07 02:45:57 +0530

    Stats

--- Web-site/forntend/src/components/Business.jsx
+++ Web-site/forntend/src/components/Business.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-
+import { features } from '../constants'
+import styles from '../style'
+import Button from './Button'
 export default function Business() {
   return (
     <div>Business</div>
